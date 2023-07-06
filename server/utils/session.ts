3cbd1f3a79191913153ceb9e2c79d95c307396b1
Luca Lucidera <lucidera.luca@gmail.com>
2023-07-06 11:41:22 +0200

    Aggiunti commenti e spazi per handleSecurity

--- server/utils/session.ts
+++ server/utils/session.ts
@@ -15,29 +15,46 @@ export async function handleSecurity(
   const AuthHeader = getHeader(event, "Authorization");
   console.log("AuthHeader", AuthHeader);
 
+  //controllo che nella richiesta sia presente
   if (AuthHeader) {
     const token = AuthHeader.split("Bearer ")[1];
+
+    //controllo che il token sia presente
     if (token != "" || token != null || token != "null" || token != undefined) {
+
+      //controllo la validità del token e lo decodifica se valido
       const resultAccess = verifyJwtAndDecode(token);
+
+      //controllo che sia un token valido
       if (resultAccess.success) {
+
+        //il token è valido e prende l'id dell'utente
         const userId = resultAccess.successData;
+
+        //controllo che non ci siano stati errori nel prendere l'id
         if (!userId) {
           return {
             success: false,
             errorData: "Token is invalid",
           };
         }
+
+        //prendo le altre infomazioni dell'utente
         const user = await prisma.user.findUnique({
           where: {
             id: userId,
           },
         });
+
+        //caso di errore ma poco probabile
         if (!user) {
           return {
             success: false,
             errorData: "User not found",
           };
         }
+
+        //tutto a posto
         return {
           success: true,
           successData: {
@@ -54,8 +71,18 @@ export async function handleSecurity(
         };
       }
     }
+
+    /**
+      se il token non è presente procede con l'esecuzione sottostante
+      esempio: utente appena apre il sito non ha l'access token, ma ha il refresh token
+      bypassa l'if di prima e fa direttamente i passaggi dopo
+    */
   }
+
+  //prendo il jwt di refresh
   const refreshToken = getCookie(event, "refresh_token");
+
+  //se non è presente vuol dire che la sessione è scaduta o non è loggato
   if (!refreshToken) {
     return {
       success: false,
