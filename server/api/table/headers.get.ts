83467000d233ea8c6ca676d11e6bddefa470d6d5
Luca Lucidera <lucidera.luca@gmail.com>
2023-07-11 14:41:55 +0200

    Aggiornato git ignore, aggiunto inizio handling error di prisma

--- server/api/table/headers.get.ts
+++ server/api/table/headers.get.ts
@@ -1,4 +1,21 @@
+import { Prisma } from "@prisma/client"
 export default defineEventHandler(async (event) => {
-  const tableHeaders = await prisma.tableHeaders.findMany();
-  return tableHeaders;
+  try {
+    return await prisma.tableHeaders.findMany();
+  } catch (e) {
+
+    if(e instanceof Prisma.PrismaClientKnownRequestError) {
+      throw handleKnownPrismaError(e);
+    }
+
+    if(e instanceof Prisma.PrismaClientRustPanicError) {
+      return handleRustPanicError();
+    }
+
+    if(e instanceof Prisma.PrismaClientUnknownRequestError) {
+      return handleUnknownPrismaError();
+    }
+
+    throw createError({ statusCode: 500, statusMessage: "Internal server error, sorry for the inconvenience" });
+  }
 });
