90d0dd9d676cfc3a0c7511b61a370c1ae20f7e48
Luca-Lucidera <82151241+Luca-Lucidera@users.noreply.github.com>
2023-07-15 13:41:55 +0200

    debug string

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import CustomFilter from "~/components/custom-filter.vue";
-
 //state
 const errors = useState<string[]>(() => []);
 
 //type, status, publisher
-const filters = useState(() => ["All", "All", "All"])
+const filters = useState(() => ["All", "All", "All"]);
 
 //store
 const tableStore = useTableStore();
@@ -18,21 +16,21 @@ onMounted(async () => {
   const resultFilters = await tableStore.fetchBooksTableFilters();
   if (!resultFilters.success) {
     if (resultFilters.errorData) {
-      errors.value.push(resultFilters.errorData)
+      errors.value.push(resultFilters.errorData);
     }
   }
 
   const resultHeaders = await tableStore.fetchBooksTableHeaders();
   if (!resultHeaders.success) {
     if (resultHeaders.errorData) {
-      errors.value.push(resultHeaders.errorData)
+      errors.value.push(resultHeaders.errorData);
     }
   }
 
   const resultBooks = await booksStore.fetchBooks();
   if (!resultBooks.success) {
     if (resultBooks.errorData) {
-      errors.value.push(resultBooks.errorData)
+      errors.value.push(resultBooks.errorData);
     }
   }
 
@@ -42,35 +40,32 @@ onMounted(async () => {
 
 <template>
   <template v-if="errors.length != 0">
-    <p v-for="error in errors" class="text-red"> {{ error }}</p>
+    <p v-for="error in errors" class="text-red">{{ error }}</p>
   </template>
   <VContainer v-else class="h-100">
+    {{ tableStore.filters }}
     <VRow align="center" class="h-100">
       <VCol cols="2">
-        <CustomFilter
-            v-if="tableStore.filters"
-            v-for="(filterEntries, i) in Object.entries(tableStore.filters)"
-            :filters="filterEntries[1]"
-            :label="filterEntries[0]"
-            v-model="filters[i]"
+        <VProgressCircular
+          v-if="tableStore.filters == null"
+          :indeterminate="true"
+          size="100"
         />
-        <div v-else>
-          caricamento.... (da eliminare)
-        </div>
+        <CustomFilter
+          v-else
+          v-for="(filterEntries, i) in Object.entries(tableStore.filters)"
+          :filters="filterEntries[1]"
+          :label="filterEntries[0]"
+          v-model="filters[i]"
+        />
       </VCol>
 
       <VCol>
         <BooksTable
-            :books="
-            booksStore.filteredBooks(
-              filters[0],
-              filters[1],
-              filters[2]
-            )"
-            :headers="tableStore.getHeaders"
+          :books="booksStore.filteredBooks(filters[0], filters[1], filters[2])"
+          :headers="tableStore.getHeaders"
         />
       </VCol>
     </VRow>
   </VContainer>
 </template>
-
